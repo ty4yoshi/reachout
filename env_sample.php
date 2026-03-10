5471572e3c58a74ead2266c8c8f3594b1a363995
Yoshihiro Takada <takada.yoshi@kyowa-concrete.co.jp>
2026-03-10 15:25:10 +0900

    キャッシュ設定の整形と不要な改行の削除

--- env_sample.php
+++ env_sample.php
@@ -4,28 +4,23 @@
 // スクリプト情報
 define('SCRIPT_VER', '0.2.0');
 define('SCRIPT_MODIFIED', '2026/03/07');
-
 // ディレクトリ設定
 define('DS', '/');
 define('LAYOUT_DIR', 'layout');
 define('CSS_DIR', 'css');
 define('CACHE_DIR', 'tmp');
-
 // サイト設定
 define('SITE_NAME', 'ReachOut - Framework For HTML');
 define('TOP_PAGE', 'top.php');
 define('LAYOUT_DEFAULT_FILE', 'default.php');
 define('HEADER_FILE', 'header.php');
 define('CHARSET', 'UTF-8');
-
 // CSSタグテンプレート
 define('CSS_TAG', '<link rel="stylesheet" href="%s">');
-
 // キャッシュ設定
 define('CACHE_FLG', false);
 define('CACHE_LIFE_TIME', 7200);
 define('CACHE_STOP', false);
-
+define('NO_CACHE_DIR', array('feed'));
 // ファイル設定
 define('INDEX_FILES', array('index.html', 'index.php', 'index.htm'));
-define('NO_CACHE_DIR', array('feed'));
